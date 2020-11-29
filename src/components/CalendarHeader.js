686a967e5eb53fae1e71979f5635bc42d054d109
Marino Kolaric <marinokolaric@gmail.com>
2020-11-29 18:55:33 +0100

    Make CalendarHeader responsive

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,25 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import { AuthContext } from "../context";
 import { theme } from "../style";
 import { Countdown } from "./Countdown";
 
 const CalendarHeaderComponent = styled.div`
   width: 100%;
-  height: 80px;
   color: #fff;
   padding: 20px 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  overflow: hidden;
+
+  flex-direction: column;
+  text-align: center;
+
+  ${theme.mediaQueries("tablet")`
+    flex-direction: row;
+  `}
 `;
 
 const Title = styled.h1`
-  font-size: 44px;
+  font-size: 22px;
   font-weight: bold;
   color: ${theme.color.gray4};
   font-family: "Concert One", cursive;
   letter-spacing: 10px;
+
+  ${theme.mediaQueries("tablet")`
+    font-size: 44px;
+  `}
 `;
 
 export const CalendarHeader = () => {
