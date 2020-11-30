3fe2df1cb5536df90717a9640bdcc4059b51b3c7
Marino Kolaric <marinokolaric@gmail.com>
2020-11-30 23:13:57 +0100

    Add endDate

--- src/components/DayModal.js
+++ src/components/DayModal.js
@@ -137,8 +137,7 @@ export const DayModal = ({ handleOpen }) => {
   };
 
   useEffect(() => {
-    // const endDate = new Date(`12/${day.day}/2020`);
-    const endDate = new Date(`11/${day.day}/2020`);
+    const endDate = new Date(`12/${day.day}/2020`);
     setDate(remainingTime(endDate));
   }, [day]);
 
